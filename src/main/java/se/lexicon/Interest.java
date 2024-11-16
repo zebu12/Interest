bb1b05ef4d766931d5c787495ceb2ec84e634bf9
zebu12 <jeanpierre.salum@outlook.com>
2024-11-17 00:56:08 +0100

    first commit

--- src/main/java/se/lexicon/Interest.java
+++ src/main/java/se/lexicon/Interest.java
@@ -1,7 +1,27 @@
 package se.lexicon;
 
-public class Main {
+import java.util.Scanner;
+
+public class Interest {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double amount;
+        double rate = 0.05;
+        double depositAmount;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount you would like to deposit: ");
+        depositAmount = scanner.nextDouble();
+        System.out.print("Enter number of the year would like to save: ");
+        int n = scanner.nextInt();
+
+
+
+        for (int year = 1; year <= n; year++) {
+
+            amount = depositAmount * Math.pow(1 + rate, year);
+            System.out.println("Year " + year + ": " + amount);
+        }
+        System.out.println();
     }
+
 }
